1adc23f7c95fa6254cf4d01be5ed281025f56300
armatron <amri.sannang@gmail.com>
2012-02-05 10:40:24 +1100

    Initial commit.

---
+++ js/bookmarker.js
@@ -0,0 +1,103 @@
+(function($){
+    $(function() {
+        /**
+         * Bookmark model.
+         */
+        window.Bookmark = Backbone.Model.extend({
+            defaults: function() {
+                return {
+                    label: '',
+                    url:   '',
+                    order: Bookmarks.nextOrder()
+                }
+            }
+        });
+
+
+        /**
+         * Bookmark collection - should save bookmarks to a local file.
+         */
+        window.BookmarkList = Backbone.Collection.extend({
+            model: Bookmark,
+            comparator: function(bookmark) {
+                return bookmark.get('order')
+            },
+            nextOrder: function() {
+                return (this.length == 0) ? 1 : this.comparator(this.last())+1;
+            }
+        });
+        window.Bookmarks = new BookmarkList();
+
+
+        /**
+         * A view for a list of bookmarks.
+         */
+        window.BookmarkerView = Backbone.View.extend({
+            el: $('#bookmark_list'),
+            events: {
+                'click .add' : 'addOne'
+            },
+            collection: Bookmarks,
+            initialize: function() {
+                this.collection.on('add',   this.addOne, this);
+                this.collection.on('reset', this.addAll, this);
+            },
+            addOne: function() {
+                var view = new BookmarkView({model: new Bookmark()});
+                this.$('.bookmarks').append(view.render().el);
+                return false;
+            },
+            addAll: function() {
+                Bookmarks.each(this.addOne);
+            }
+        });
+        window.Bookmarker = new BookmarkerView();
+
+
+        /**
+         * Bookmark view - shows a single bookmark.
+         */
+        window.BookmarkView = Backbone.View.extend({
+            tagName: 'li',
+            template: Bookmarker.$('.template'),
+            events: {
+                'submit .save_form' : 'saveBookmark',
+                'click .edit'       : 'editBookmark',
+                'click .delete'     : 'deleteBookmark'
+            },
+            initialize: function() {
+                this.model.on('change',  this.render, this);
+                this.model.on('destroy', this.remove, this);
+            },
+            render: function() {
+                //
+                // Set the template's contents based on what's in the model.
+                var this_view = this.$el.html(this.template.clone()).removeClass('template');
+                this_view.find('.display .bookmark_link')
+                    .attr('href', this.model.get('url'))
+                    .text(this.model.get('label'));
+                this_view.find('.label_input :input').val(this.model.get('label'));
+                this_view.find('.url_input :input').val(this.model.get('url'));
+                return this;
+            },
+            saveBookmark: function() {
+                this.model.save({
+                    label: this.$('.label_input :input').val(),
+                    url:   this.$('.url_input :input').val()
+                });
+                this.$el.removeClass('editing');
+                return false;
+            },
+            editBookmark: function() {
+                this.$el.addClass('editing');
+                this.$('.label_input :input').focus();
+                return false;
+            },
+            deleteBookmark: function() {
+                this.remove();
+                this.model.destroy();
+                return false;
+            }
+        });
+    });
+})(jQuery);
