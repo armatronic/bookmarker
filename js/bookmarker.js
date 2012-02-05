cead458ecd54a34e2b0ce658090d27ba0adec0f5
armatron <amri.sannang@gmail.com>
2012-02-05 23:18:39 +1100

    Added local file storage.

--- js/bookmarker.js
+++ js/bookmarker.js
@@ -18,53 +18,21 @@
         /**
          * Bookmark collection - should save bookmarks to a local file.
          */
+        var file_path = document.location.href.replace(/([\///])[^\///]+$/i, '$1items.json');
+//        file_path     = $.twFile.convertUriToLocalPath(file_path);
         window.BookmarkList = Backbone.Collection.extend({
             model: Bookmark,
+            fileStorage: new FileStore($.twFile.convertUriToLocalPath(file_path)),
             comparator: function(bookmark) {
                 return bookmark.get('order')
             },
             nextOrder: function() {
-                return (this.length == 0) ? 1 : this.comparator(this.last())+1;
+//                return (this.length == 0) ? 1 : (this.last().get('order')+1);
+//                return (this.length == 0) ? 1 : (this.comparator(this.last())+1);
+                return this.length + 1;
             }
         });
         window.Bookmarks = new BookmarkList();
-
-
-        /**
-         * A view for a list of bookmarks.
-         */
-        window.BookmarkerView = Backbone.View.extend({
-            el: $('#bookmark_list'),
-            events: {
-                'click .add'       : 'addOne',
-                'click .save_all'  : 'saveAll',
-                'click .tag'       : 'showForTag',
-                'click .clear_tag' : 'render'
-            },
-            collection: Bookmarks,
-            initialize: function() {
-                this.collection.on('add',   this.addOne, this);
-                this.collection.on('reset', this.addAll, this);
-            },
-            addOne: function() {
-                var view = new BookmarkView({model: new Bookmark()});
-                view.editBookmark();
-                this.$('.bookmarks').append(view.render().el);
-                return false;
-            },
-            addAll: function() {
-                Bookmarks.each(this.addOne);
-            },
-            saveAll: function() {
-                //
-                // Use tiddly thing to save changes.
-                return false;
-            },
-            showForTag: function() {
-                return false;
-            }
-        });
-        window.Bookmarker = new BookmarkerView();
 
 
         /**
@@ -72,7 +40,7 @@
          */
         window.BookmarkView = Backbone.View.extend({
             tagName: 'li',
-            template: Bookmarker.$('.template').html(),
+            template: $('#bookmark_list .template').html(),
             events: {
                 'submit .save_form' : 'saveBookmark',
                 'click .cancel'     : 'cancelEdit',
@@ -86,8 +54,10 @@
             render: function() {
                 //
                 // Set the template's contents based on what's in the model.
-                var this_view = this.$el.html(this.template).removeClass('template').addClass('bookmark');
-                this_view.find('.display .bookmark_link')
+                if (this.$el.children().length == 0) {
+                    this.$el.html(this.template).removeClass('template').addClass('bookmark');
+                }
+                this.$('.display .bookmark_link')
                     .attr('href', this.model.get('url'))
                     .text(this.model.get('label'));
 
@@ -95,10 +65,10 @@
                 var tag_links = _.map(tags, function(tag) {
                     return $('<a href="#" class="tag" />').text(tag).wrap('<p />').parent().html();
                 }).join(', ');
-                this_view.find('.display .tags').append(tag_links);
-                this_view.find('.label_input :input').val(this.model.get('label'));
-                this_view.find('.url_input :input').val(this.model.get('url'));
-                this_view.find('.tag_input :input').val(tags.join(' '));
+                this.$('.display .tags').empty().append(tag_links);
+                this.$('.label_input :input').val(this.model.get('label'));
+                this.$('.url_input :input').val(this.model.get('url'));
+                this.$('.tag_input :input').val(tags.join(' '));
                 return this;
             },
             saveBookmark: function() {
@@ -109,6 +79,7 @@
                     url:   this.$('.url_input :input').val(),
                     tags:  this.$('.tag_input :input').val().split(/\s+/)
                 });
+//                this.model.save();
                 this.$el.removeClass('editing');
                 this.render();
                 return false;
@@ -128,5 +99,47 @@
                 return false;
             }
         });
+
+
+        /**
+         * A view for a list of bookmarks.
+         */
+        window.BookmarkerView = Backbone.View.extend({
+            el: $('#bookmark_list'),
+            events: {
+                'click .add'       : 'add',
+                'click .save_all'  : 'saveAll',
+                'click .tag'       : 'showForTag',
+                'click .clear_tag' : 'render'
+            },
+            collection: Bookmarks,
+            initialize: function() {
+                this.collection.on('add',   this.addOne, this);
+                this.collection.on('reset', this.addAll, this);
+                this.collection.fetch();
+            },
+            add: function() {
+                this.collection.create({});
+            },
+            addOne: function(bookmark) {
+                var view = new BookmarkView({model: bookmark});
+                view.editBookmark();
+                this.$('.bookmarks').append(view.render().el);
+                return false;
+            },
+            addAll: function() {
+                Bookmarks.each(this.addOne);
+            },
+            saveAll: function() {
+                //
+                // Use tiddly thing to save changes.
+//                this.collection.save();
+                return false;
+            },
+            showForTag: function() {
+                return false;
+            }
+        });
+        window.Bookmarker = new BookmarkerView();
     });
 })(jQuery);
