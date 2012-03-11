a341d2a425b925824202f8129bb47b880fb3af93
armatron <amri.sannang@gmail.com>
2012-03-11 23:11:19 +1100

    Updated bookmarker.init to conform to JsLint use strict.

--- js/bookmarker.init.js
+++ js/bookmarker.init.js
@@ -1,14 +1,16 @@
-(function($){
-    $(function(){
+var window = window || {};
+(function ($) {
+    "use strict";
+    $(function () {
         //
         // Any browser that uses the jar file for load/save won't load the jar
         // until all ready events have run.
         // The easiest way around this is to put the load in a setTimeout()
         // object, which won't run until the callbacks have finished.
-        setTimeout(function() {
+        window.setTimeout(function () {
             $('#bookmark_list').bookmarker({
                 file_path: 'items.json'
             });
         });
     });
-})(jQuery);
+}(window.jQuery));
