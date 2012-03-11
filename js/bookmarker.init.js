8334a1d7db7f5831f5355747bdc5247129ce125e
armatron <amri.sannang@gmail.com>
2012-03-11 17:57:07 +1100

    Deferred loading of bookmarker by putting it into a setTimeout() block.
    This fixes issue with using TiddlySaver loader - it no longer attempts to set up the bookmarker before the file reader is set up.

--- js/bookmarker.init.js
+++ js/bookmarker.init.js
@@ -1,7 +1,14 @@
 (function($){
     $(function(){
-        $('#bookmark_list').bookmarker({
-            file_path: 'items.json'
+        //
+        // Any browser that uses the jar file for load/save won't load the jar
+        // until all ready events have run.
+        // The easiest way around this is to put the load in a setTimeout()
+        // object, which won't run until the callbacks have finished.
+        setTimeout(function() {
+            $('#bookmark_list').bookmarker({
+                file_path: 'items.json'
+            });
         });
     });
 })(jQuery);
